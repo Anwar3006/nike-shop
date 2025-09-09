aacd9b4c2269737e18c675c5e9fed637023b9c58
Anwar3006 <anwarsadat.d2@gmail.com>
2025-09-09 14:14:16 +0000

    fix: backend build

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,10 +1,10 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
-import { DATABASE_URL } from "./src/config/default";
+import { DATABASE_URL } from "./src/config/default.js";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/models/index.ts",
+  schema: "./src/models/index.js",
   dialect: "postgresql",
   dbCredentials: {
     url: DATABASE_URL!,
