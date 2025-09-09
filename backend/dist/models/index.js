a1485e4a10d00e0bd8465c29ec4673262ccbb678
Anwar3006 <anwarsadat.d2@gmail.com>
2025-09-09 16:06:55 +0000

    fix: backend build - 4

--- backend/dist/models/index.js
+++
@@ -1,5 +0,0 @@
-export { user, address, account, session, verification } from "./auth-model.js";
-export { category, shoes, sizes, shoeSizes, colorVariant, images, categoriesRelations, shoeSizesRelations, sizesRelations, imagesRelations, shoesRelations, colorVariantRelations, } from "./shoes.model.js";
-export { search_queries, search_clicks, popular_searches, } from "./search.model.js";
-export { favorites, favoritesRelations } from "./favorites.model.js";
-export * from "./orders.model.js";
--- backend/dist/utils/slugify.js
+++
@@ -1,7 +0,0 @@
-import slugify from "slugify";
-export const createSlug = (text) => {
-    return slugify(text, {
-        lower: true,
-        strict: true,
-    });
-};
