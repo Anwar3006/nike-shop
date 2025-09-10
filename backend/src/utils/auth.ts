eb19140a952d0338b1c817633c163db5558b681c
Anwar3006 <anwarsadat.d2@gmail.com>
2025-09-10 07:55:57 +0000

    fix: navbar userSession not showing after oauth sign-in - changed cookies-sameSite from strict to none to attempt to address the frontend session issue

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -50,7 +50,7 @@ export const auth = betterAuth({
         options: {
           httpOnly: true,
           secure: process.env.NODE_ENV === "production",
-          sameSite: "strict",
+          sameSite: "none",
           path: "/",
           maxAge: 60 * 60 * 24 * 7,
         },
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -28,25 +28,25 @@ const Navbar = () => {
   const { data, isPending } = useSession();
   const user = data?.user;
 
-  console.log("@@session: ", data);
+  // console.log("@@session: ", authClient.getSession());
   console.log("@@isPending: ", isPending);
   console.log("@@user: ", user);
 
   // Force session refresh on mount (useful after OAuth redirects)
-  useEffect(() => {
-    const refreshSession = async () => {
-      try {
-        await authClient.getSession();
-      } catch (error) {
-        console.error("Failed to refresh session:", error);
-      }
-    };
+  // useEffect(() => {
+  //   const refreshSession = async () => {
+  //     try {
+  //       await authClient.getSession();
+  //     } catch (error) {
+  //       console.error("Failed to refresh session:", error);
+  //     }
+  //   };
 
-    // Only refresh if we don't have a user but we're not loading
-    if (!user && !isPending) {
-      refreshSession();
-    }
-  }, [user, isPending]);
+  //   // Only refresh if we don't have a user but we're not loading
+  //   if (!user && !isPending) {
+  //     refreshSession();
+  //   }
+  // }, [user, isPending]);
 
   const routeTo = createRedirectUrl(path, "sign-in");
   const toggleMenu = () => {
